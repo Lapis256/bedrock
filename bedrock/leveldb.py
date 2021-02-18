a4873e488809bdca441d0eb978c2b503c1e557d3
lapis256 <lapis128.256@gmail.com>
2021-02-19 08:35:56 +0900

    support android

--- bedrock/leveldb.py
+++ bedrock/leveldb.py
@@ -4,12 +4,7 @@ import ctypes
 import os.path as op
 import sys
 
-if sys.platform == "win32":
-  if sys.maxsize > 2**32: # 64 bit python
-    ldb = ctypes.cdll.LoadLibrary(op.join(op.dirname(op.realpath(__file__)), "LevelDB-MCPE-64.dll"))
-  else: # 32 bit python
-    ldb = ctypes.cdll.LoadLibrary(op.join(op.dirname(op.realpath(__file__)), "LevelDB-MCPE-32.dll"))
-else: #linux, compile your own .so if this errors!
+if sys.platform != "win32":
   ldb = ctypes.cdll.LoadLibrary(op.join(op.dirname(op.realpath(__file__)), "libleveldb.so")) # Load DLL
 
 # Setup ctypes arguments and return types for all of the leveldb functions.
