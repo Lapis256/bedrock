e18ace8712b24f65f1774665eee9c246604d449d
Lapis <45414333+Lapis256@users.noreply.github.com>
2021-02-19 15:19:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(name="bedrock",
       version="0.1",
       description="A simply python library to access Minecraft: Bedrock Edition worlds.",
       keywords="minecraft bedrock leveldb",
-      url="https://github.com/BluCodeGH/bedrock",
+      url="https://github.com/Lapis256/bedrock",
       packages=["bedrock"],
       install_requires=["numpy"],
       package_data={
-          "bedrock": ["*.dll", "*.so", "LICENCE-LEVELDB"]
+          "bedrock": ["*.so", "LICENCE-LEVELDB"]
       },
-      author="BluCode")
+      author="Lapis256")
